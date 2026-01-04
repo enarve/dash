7eb7cc9a38f27ddfb8cd795f06317a0f1c798f52
Nikita Vlaskin <sinezeleuny@gmail.com>
2026-01-04 21:42:45 +0100

    Crossterm dependency added

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ use std::io::{self, Read};
 
 fn main() {
     for b in io::stdin().bytes() {
+        println!("{:?}", b);
         let c = b.unwrap() as char;
         println!("{}", c);
+        if c == 'q' {
+            break;
+        }
     }
 }
